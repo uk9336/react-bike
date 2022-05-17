d1ec2a1b1017bbc1f7ee14b8871a907a3913bfd1
이지원 <uk9336@naver.com>
2022-05-17 22:26:30 +0900

    * 타이틀 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MapAPI from './component/MapAPI';
 import Test from './component/Test';
+import Title from './component/Title';
 
 // redux
 import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+
   const dispatch = useDispatch()
 
   const { value } = useSelector(state => state.value)
@@ -25,22 +27,23 @@ function App() {
   }
 
   return (
-    // <div className="App">
-    //   <MapAPI />
-    // </div>
-
     <div className="App">
-      <div>
-        value: {value}
-      </div>
-      <button onClick={addValue}> + </button>
-      <button onClick={subValue}> - </button>
-      <button onClick={resetValue}> reset </button>
-      <div>
-        count: {count}
-      </div>
-      <button onClick={pushButton}> click </button>
+      <Title />
+      <MapAPI />
     </div>
+
+    // <div className="App">
+    //   <div>
+    //     value: {value}
+    //   </div>
+    //   <button onClick={addValue}> + </button>
+    //   <button onClick={subValue}> - </button>
+    //   <button onClick={resetValue}> reset </button>
+    //   <div>
+    //     count: {count}
+    //   </div>
+    //   <button onClick={pushButton}> click </button>
+    // </div>
   );
 }
 export default App;
---
+++ src/component/Title.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../css/title.css";
+
+export default function Title() {
+  return <h1 className="title_container">자전거 대여소 정보</h1>;
+}
